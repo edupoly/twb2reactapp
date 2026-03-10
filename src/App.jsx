e90dd05bb43ec5ed43e9c8899b441e8e2e341447
edupoly <info.edupoly@gmail.com>
2026-03-10 13:56:41 +0530

    apicalls

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./component/Navbar";
 import Counter from "./features/Counter";
+import Products from "./features/Products";
 function App() {
   return (
-    <div>
+    <div className="border rounded border-danger p-3 m-3">
       <Navbar></Navbar>
       <h1>Hello App</h1>
+      <Products></Products>
     </div>
   );
 }
