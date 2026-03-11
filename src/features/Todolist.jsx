fba9ab4297984139b420076b96827402ff902db9
edupoly <info.edupoly@gmail.com>
2026-03-11 13:04:52 +0530

    setter-callback-todolist

--- src/features/Todolist.jsx
+++ src/features/Todolist.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
 function Todolist() {
-  const [todos, setTodos] = React.useState([]);
+  const [todos, setTodos] = React.useState([
+    "goto goa",
+    "watch movie",
+    "play cricket",
+  ]);
   const [ntd, setNtd] = React.useState("");
   function addTodo() {
-    setTodos([...todos, ntd]);
+    // setTodos([...todos, ntd]);
+    setTodos((ctodos) => {
+      ctodos.push(ntd);
+      return [...ctodos];
+    });
+  }
+  function handleDelete(ind) {
+    setTodos((ctodos) => {
+      ctodos.splice(ind, 1);
+      return [...ctodos];
+    });
   }
   return (
-    <div>
+    <div className="p-2 m-2 border border-2 rounded">
       <h1>Todo List</h1>
       <input
         type="text"
@@ -23,8 +37,19 @@ function Todolist() {
         Add Todo
       </button>
       <ul>
-        {todos.map((todo) => {
-          return <li>{todo}</li>;
+        {todos.map((todo, i) => {
+          return (
+            <li className="border border-2 border-success rounded m-2 p-2">
+              {todo}
+              <button
+                onClick={() => {
+                  handleDelete(i);
+                }}
+              >
+                Delete
+              </button>
+            </li>
+          );
         })}
       </ul>
     </div>
