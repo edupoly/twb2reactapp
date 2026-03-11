0d77ed31e4da87cb3c2f5b15aa86bbdc7e7f8f3e
edupoly <info.edupoly@gmail.com>
2026-03-11 12:52:09 +0530

    setter-callback-counter

--- src/features/Counter.jsx
+++ src/features/Counter.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import Button from "@mui/material/Button";
 function ButtonUsage(props) {
-  return <Button variant="contained">{props.children}</Button>;
+  return;
 }
 function Counter() {
   const [count, setCount] = React.useState(0);
-  function inc(){
-    setCount(count+1)
+  function inc() {
+    // setCount(count + 1);
+    setCount((cv) => {
+      return cv + 1;
+    });
+  }
+  function dec() {
+    setCount(count - 1);
   }
   return (
     <div className="mybox">
       <h1>Counter:{count}</h1>
-      <ButtonUsage>Increment</ButtonUsage>
-      <ButtonUsage>Decrement</ButtonUsage>
+      <Button variant="contained" onClick={inc}>
+        Increment
+      </Button>
+      <Button variant="contained" onClick={dec}>
+        Decrement
+      </Button>
     </div>
   );
 }
