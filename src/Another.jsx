8b361cb7eed98dc43793d99c6f14468c65535124
edupoly <info.edupoly@gmail.com>
2026-03-11 14:30:33 +0530

    adv-refs

---
+++ src/Another.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Another() {
+  return (
+    <div className="m-5 p-5 border border-4 border-success rounded">
+      <h1>Another</h1>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default Another;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
+import { useEffect, useRef } from "react";
 import Navbar from "./component/Navbar";
 import Counter from "./features/Counter";
+import Another from "./Another";
 function App() {
+  const ref1 = useRef();
+  const ref2 = useRef();
+  useEffect(() => {
+    ref1.current.focus();
+  }, []);
+  function handleRef1(ev) {
+    if (ev.key === "Enter") {
+      ref2.current.focus();
+    }
+  }
   return (
-    <div>
+    <div className="m-5 p-5 border border-2 border-danger rounded">
       <Navbar></Navbar>
       <h1>Hello App</h1>
+      <input
+        type="text"
+        ref={ref1}
+        onKeyUp={(e) => {
+          handleRef1(e);
+        }}
+      />
+      <br />
+      <br />
+      <br />
+      <input type="text" ref={ref2} />
+      <Another></Another>
     </div>
   );
 }
