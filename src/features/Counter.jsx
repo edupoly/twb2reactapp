36f1ce53da9ca1634a9ae0e8eb0e4184e8aeaa73
edupoly <info.edupoly@gmail.com>
2026-03-11 12:42:58 +0530

    useEffect-ex1

--- src/features/Counter.jsx
+++ src/features/Counter.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 function ButtonUsage(props) {
   return <Button variant="contained">{props.children}</Button>;
 }
 function Counter() {
-  const [count, setCount] = React.useState(0);
-  function inc(){
-    setCount(count+1)
+  const [count, setCount] = useState(0);
+  function inc() {
+    setCount(count + 1);
   }
   return (
     <div className="mybox">
